d420a05f0d53785c2c020019cec3152bdce4fd3f
Rishabh <rishabhyadav0227@gmail.com>
2025-07-30 23:34:42 +0530

    changed

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
 
+btn.innerText='Change Color Backgroud color'
+btn.style.padding='10px';
+btn.style.borderRadius='5px';
+btn.style.border='2px blue solid';
+btn.style.margin='10px'
+btn.style.color='white'
+btn.style.backgroundColor='transparent'
+
 
 btn.addEventListener("click", function () {
         let hexcolor = '#'
